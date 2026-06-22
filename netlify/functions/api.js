619f73749f08731b569aee58d194ee4a2a00a822
Abirami Shankari <abiramishankari@example.com>
2026-06-22 13:25:05 +0530

    add /test-env debug endpoint

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,15 @@
 const serverless = require('serverless-http');
 const { app } = require('../../backend/server.js');
 
+app.get('/test-env', (req, res) => {
+  res.json({
+    url: typeof process.env.DATABASE_URL,
+    urlValue: process.env.DATABASE_URL ? 'set' : 'not set',
+    direct: typeof process.env.DIRECT_URL,
+    jwt: typeof process.env.JWT_SECRET
+  });
+});
+
 module.exports.handler = serverless(app, {
   basePath: '/.netlify/functions'
 });
